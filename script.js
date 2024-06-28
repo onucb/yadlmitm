ec9e270b38fa91da09fda30804e035b44c65478f
onucb <98803886+onucb@users.noreply.github.com>
2024-06-28 11:36:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-$notification.post("Обнаружен трек","","Нажмите, чтобы скачать трек с id "+$request.url.replace(/(.*track-id=|&.*)/g, ""),"shortcuts://run-shortcut?name=Я.музыка загрузчик&input=text&text="+$request.url)
+$notification.post("Обнаружен трек","","Нажмите, чтобы скачать трек с id "+$request.url.replace(/(.*track-id=|&.*)/g, ""),"shortcuts://run-shortcut?name=yadlmitm&input=text&text="+$request.url)
 
-console.log("shortcuts://run-shortcut?name=Я.музыка загрузчик&input=text&text="+$request.url)
+console.log("shortcuts://run-shortcut?name=yadlmitm&input=text&text="+$request.url)
